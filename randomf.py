a4bc2de2dee7b26eaa366818abd340534797e946
yogita <yogita@yogitas-mbp.home>
2023-11-06 01:10:23 +0000

    Task 4

--- randomf.py
+++ randomf.py
@@ -1,6 +1,6 @@
 # import random
 import random as rd
-
+import string
 # List containing the names of my 5 favourite fruits
 word_list = ['strawberry', 'kiwi', 'grapes', 'banana', 'orange']
 # Print out the created list
@@ -9,3 +9,11 @@ print(word_list)
 
 word = rd.choice(word_list)
 print(word) 
+
+print("Please enter a single letter input: ")
+guess = input()
+if len(guess) == 1 and guess.lower() in list(string.ascii_lowercase):
+    print("Good guess")
+else:
+    print("Oops! That is not a valid input.")
+
