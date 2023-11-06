18a1f29d3bfb5aeae3f04f7c13cfa216ce5fefd3
yogita <yogita@yogitas-mbp.home>
2023-11-06 00:59:18 +0000

    task 2

---
+++ randomf.py
@@ -0,0 +1,11 @@
+# import random
+import random as rd
+
+# List containing the names of my 5 favourite fruits
+word_list = ['strawberry', 'kiwi', 'grapes', 'banana', 'orange']
+# Print out the created list
+print(word_list)
+
+
+word = rd.choice(word_list)
+print(word) 
